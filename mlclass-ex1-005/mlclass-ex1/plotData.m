801b03a691f79241e955c2d377a1d37a4271cc6a
vitamin <wangwei200508@gmail.com>
2014-03-11 01:07:31 +0800

    finish 2/7 of ex1

--- mlclass-ex1-005/mlclass-ex1/plotData.m
+++ mlclass-ex1-005/mlclass-ex1/plotData.m
@@ -3,6 +3,10 @@ function plotData(x, y)
 %   PLOTDATA(x,y) plots the data points and gives the figure axes labels of
 %   population and profit.
 
+plot(x, y, 'rx', 'MarkerSize', 10);
+ylabel('Profit in $10,000s');
+xlabel('Population of City in 10,000s');
+
 % ====================== YOUR CODE HERE ======================
 % Instructions: Plot the training data into a figure using the 
 %               "figure" and "plot" commands. Set the axes labels using
