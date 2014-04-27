cc3e0a39c0f078af2d81bf675f1ef2da73c49865
vitamin <wangwei200508@gmail.com>
2014-04-27 23:15:28 +0800

    finish ex2

--- mlclass-ex2-005/mlclass-ex2/costFunctionReg.m
+++ mlclass-ex2-005/mlclass-ex2/costFunctionReg.m
@@ -17,10 +17,10 @@ grad = zeros(size(theta));
 %               Compute the partial derivatives and set grad to the partial
 %               derivatives of the cost w.r.t. each parameter in theta
 
-
-
-
-
+h = sigmoid(X*theta);
+theta(1)=0;
+J = (1/m) * sum(-y.*log(h).-(1.-y).*log(1.-h))+(lambda/(2*m))*sum(theta.^2);
+grad = (1/m) .* (X'*(h-y))+(lambda/m)*theta;
 
 % =============================================================
 
