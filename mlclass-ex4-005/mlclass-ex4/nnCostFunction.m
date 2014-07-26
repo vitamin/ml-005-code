6766a02a1891d9b3de6a5d31f8e4e5515c0f9203
vitamin <wangwei200508@gmail.com>
2014-07-26 21:20:56 +0800

    all finish

--- mlclass-ex4-005/mlclass-ex4/nnCostFunction.m
+++ mlclass-ex4-005/mlclass-ex4/nnCostFunction.m
@@ -63,6 +63,41 @@ Theta2_grad = zeros(size(Theta2));
 %
 
 
+s = 0;
+for i=1:m,
+    x_i = [1 X(i,:)]';
+    
+    y_i = zeros(num_labels, 1);
+    y_i(y(i)) = 1;
+    
+    z_2 = Theta1 * x_i;
+    a_2 = [1; sigmoid(z_2)];
+    
+    z_3 = Theta2 * a_2;
+    h = sigmoid(z_3);
+    
+    d_3 = h - y_i;
+    d_2 = Theta2' * d_3;
+    d_2 = d_2(2:end) .* sigmoidGradient(z_2);
+    
+    Theta2_grad = Theta2_grad + (d_3 * a_2');
+    Theta1_grad = Theta1_grad + (d_2 * x_i');
+    
+    s = s + sum(-y_i .* log(h) .- (1.-y_i) .* log(1.-h));
+end
+
+t_1 = Theta1(:,2:end);
+t_2 = Theta2(:,2:end);
+
+Theta1_grad = [Theta1_grad(:,1)/m ((Theta1_grad(:,2:end)/m) + (lambda/m) * t_1)];
+Theta2_grad = [Theta2_grad(:,1)/m ((Theta2_grad(:,2:end)/m) + (lambda/m) * t_2)];
+
+t_1 = sum(sum(t_1.^2));
+t_2 = sum(sum(t_2.^2));
+r = (lambda/(2*m))*(t_1 + t_2);
+
+J = (1/m) * s + r;
+
 
 
 
